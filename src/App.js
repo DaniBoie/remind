76c4d2f446d0ab9339e205ac6a7c429f6b006ce9
Daniel Ayala <dannyboy3385@gmail.com>
2021-04-03 15:53:57 -0700

    useState hooks and errors

--- src/App.js
+++ src/App.js
@@ -3,19 +3,33 @@ import React, { useState } from 'react'
 
 const App = () => {
   // useState is a hook that replaces state = { } in smart components.
+  // If there is more than one variable in countstate they will disappear unless spread operater used in setState.
   const [countState, setCountState] = useState({
-    count: 0
+    count: 0,
+    message: 'hello'
   })
 
-  const handleBtnClick = () => {
-    setCountState({ count: countState.count + 1 })
+  // Any methods inside of App dealing with state should be created as a method OF state.
+  countState.handleBtnClick = event => {
+    if (event.target.value === '+') {
+      setCountState({ ...countState, count: countState.count + 1 })
+    } else {
+      setCountState({ ...countState, count: countState.count - 1 })
+    }
+
+    if (event.target.value === 'r') {
+      setCountState({ ...countState, count: 0 })
+    }
   }
 
   return (
     <>
+      <h1>{countState.message}</h1>
       <h1>Count: {countState.count}</h1>
       {/* Because we no longer are using classes, no need for the this. in front of functions or variables. */}
-      <button onClick={handleBtnClick}>Click ME!</button>
+      <button onClick={countState.handleBtnClick} value='+'>+</button>
+      <button onClick={countState.handleBtnClick} value='-'>-</button>
+      <button onClick={countState.handleBtnClick} value='r'>Reset</button>
     </>
   )
 }
